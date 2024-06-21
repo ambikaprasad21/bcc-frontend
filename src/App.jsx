a06c090710ac3607750f8f99e7e397f7d9e6e681
Ambika Prasad <akashboard2021@gmail.com>
2024-06-21 12:35:40 +0530

    disable right click

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,14 @@ import CreateNews from "./BreakingNews/CreateNews";
 import CreateJob from "./job/CreateJob";
 import JobItem from "./job/JobItem";
 
+// disable right click
+window.addEventListener("contextmenu", (e) => e.preventDefault());
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 123) e.preventDefault();
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 73) e.preventDefault();
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 74) e.preventDefault();
+});
+
 function App() {
   return (
     <AuthProvider>
